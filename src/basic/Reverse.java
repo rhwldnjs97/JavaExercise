e375acb1c12bdd249b16f1b31ebb7fe318c6d4d7
rhwldnjs97 <rhwldnjs97@naver.com>
2016-03-30 17:17:16 +0900

    Update Reverse.java

--- src/basic/Reverse.java
+++ src/basic/Reverse.java
@@ -1,23 +1,20 @@
-package basic;
-
 import java.util.Scanner;
 
-public class Reverse {
-    public static void main(String[] args) {
-        String[] strArr = new String[5];
-        Scanner scanner = new Scanner(System.in);
-        int i;
-
-        for (i = 0; i < strArr.length; i++) {
-            strArr[i] = scanner.next();
-        }
-        for (i = strArr.length - 1; i >= 0; i--) {
-            System.out.print(strArr[i] + " ");
-        }
-        System.out.println();
-    }
+public class Reverse{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		
+		int n;
+		String[] arr = new String[5];
+		
+		for(n=0; n<arr.length; n++){
+			arr[n] = s.next();
+		}
+		System.out.println();
+		
+		for(n=arr.length-1; n>=0; n--){
+			System.out.print(arr[n] + " ");
+		}
+		
+	}
 }
-/* running result
-감 호박 수박 사과 배
-배 사과 수박 호박 감 
-*/
