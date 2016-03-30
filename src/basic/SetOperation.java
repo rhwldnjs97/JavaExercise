7ce2b8eb03802b4b8adedebb9c20d1084d0fdbc5
rhwldnjs97 <rhwldnjs97@naver.com>
2016-03-30 17:18:19 +0900

    Update SetOperation.java

--- src/basic/SetOperation.java
+++ src/basic/SetOperation.java
@@ -1,17 +1,62 @@
-package basic;
-
-/**
- * Created by jyheo on 2016-03-22.
- */
 public class SetOperation {
     public static int[] union(int[] A, int[] B) {
-        // TODO: return union of set A and B
-        return new int[0];
+    	int[] C = new int[A.length + B.length];
+        int n;
+
+        for(int i=0; i<A.length; i++) {
+            C[i] = A[i];
+        }
+        n = A.length;
+
+        for(int i=0; i<B.length; i++) {
+
+            boolean TF = false;
+            
+            for(int j=0; j<A.length; j++) {
+                if(B[i] == A[j]) {
+                    TF = true;
+                    break;
+                }
+            }
+            if(TF == false) {
+                C[n] = B[i];
+                n++;
+            }
+
+        }
+
+        int[] RC = new int[n];
+        for(int i = 0; i < RC.length; i++)
+            RC[i] = C[i];
+
+        return RC;
     }
 
     public static int[] intersection(int[] A, int[] B) {
-        // TODO: return intersection of set A and B
-        return new int[0];
+    	int[] D = new int[A.length];
+    	 int n=0;
+    	 	
+         for (int i=0; i<B.length; i++) {
+
+             boolean TF = false;
+             
+             for (int j=0; j<A.length; j++) {
+                 if(B[i] == A[j]) {
+                     TF = true;
+                     break;
+                 }
+             }
+             if (TF == true) {
+                 D[n] = B[i];
+                 n++;
+             }
+
+         }
+
+         int[] RD = new int[n];
+         for (int i = 0; i < RD.length; i++)
+             RD[i] = D[i];
+    	return RD;
     }
 
     public static void print_array(int[] A) {
@@ -30,9 +75,4 @@ public class SetOperation {
         int[] D = union(A, B);
         print_array(D);
     }
-
-    /* execution result
-        3 5
-        1 2 3 4 5 7 8 9
-    */
 }
